cc75385faeebf4a2933d9794c6259bee65c174a4
z.shajarkar <z.shajarkar@fanap.ir>
2022-01-20 00:06:20 +0330

    Add Validation for job ,and define jobGroup as a list

--- src/main/java/com/example/demo/controller/JobGroupController.java
+++ src/main/java/com/example/demo/controller/JobGroupController.java
@@ -4,11 +4,13 @@ import com.example.demo.services.JobGroupService;
 import com.example.demo.util.ResponseFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/public/job-groups/v1")
 public class JobGroupController {
