012b5f44974349fbf32eaa81190cfb731f3ae8fb
z.shajarkar <z.shajarkar@fanap.ir>
2022-02-08 22:15:04 +0330

    Complete Province

--- src/main/java/com/example/demo/enums/Province.java
+++ src/main/java/com/example/demo/enums/Province.java
@@ -1,43 +1,50 @@
 package com.example.demo.enums;
 
 public enum Province {
-    AZARBAYEJAN_SHARGHI("آذر بایجان شرقی"),
-    AZARBAYEJAN_GHARBI("آذر بایجان غربی"),
-    ARDEBIL("اردبیل"),
-    ISFAHAN("اصفهان"),
-    ALBORZ("البرز"),
-    ILAM("ایلام"),
-    BUSHEHR("بوشهر"),
-    TEHRAN("تهران"),
-    CHAHAR_MAHAL_BAKHTIARI("چهارمحل بختیاری"),
-    KHORASAN_JINUBI("خرسان جنوبی"),
-    KHORASAN_SHOMALI("خراسان شمالی"),
-    KHUZESTAN("خوزستان"),
-    ZANJA("زنجان"),
-    SEMNAN("سمنان"),
-    SISTAN_BLUCHESTAN("سیستان بلوچستان"),
-    FARS("فارس"),
-    GHAZVIN("قزوین"),
-    GHOM("قم"),
-    KERMAN("کرمان"),
-    KERMANSHAH("کرمانشاه"),
-    KAHKYOYEH_VA_BOYER_AHMAD("کهگیلویه و بویراحمد"),
-    GOLESTAN("گلستان"),
-    GILAN("گیلان"),
-    LORESTAN("لرستان"),
-    MAZANDARAN("مازندران"),
-    MARKAZI("مرکزی"),
-    HORMOZGAN("هرمزگان"),
-    HAMEDAN("همدان"),
-    YAZD("یزد");
+    AZARBAYEJAN_SHARGHI("آذر بایجان شرقی",1),
+    AZARBAYEJAN_GHARBI("آذر بایجان غربی",2),
+    ARDEBIL("اردبیل",3),
+    ISFAHAN("اصفهان",4),
+    ALBORZ("البرز",5),
+    ILAM("ایلام",6),
+    BUSHEHR("بوشهر",7),
+    TEHRAN("تهران",8),
+    CHAHAR_MAHAL_BAKHTIARI("چهارمحل بختیاری",9),
+    KHORASAN_JINUBI("خرسان جنوبی",10),
+    KHORASAN_SHOMALI("خراسان شمالی",11),
+    KHUZESTAN("خوزستان",12),
+    ZANJA("زنجان",13),
+    SEMNAN("سمنان",14),
+    SISTAN_BLUCHESTAN("سیستان بلوچستان",15),
+    FARS("فارس",16),
+    GHAZVIN("قزوین",17),
+    GHOM("قم",18),
+    KERMAN("کرمان",19),
+    KERMANSHAH("کرمانشاه",20),
+    KAHKYOYEH_VA_BOYER_AHMAD("کهگیلویه و بویراحمد",21),
+    GOLESTAN("گلستان",22),
+    GILAN("گیلان",23),
+    LORESTAN("لرستان",24),
+    MAZANDARAN("مازندران",25),
+    MARKAZI("مرکزی",26),
+    HORMOZGAN("هرمزگان",27),
+    HAMEDAN("همدان",28),
+    YAZD("یزد",29);
 
     private final String value;
+    private final int id;
 
-    Province(String value) {
+    Province(String value, int id) {
         this.value = value;
+        this.id = id;
     }
+
+    public int id() {
+        return this.id;
+    }
+
 
     public String value() {
         return this.value;
     }
-    }
+}
